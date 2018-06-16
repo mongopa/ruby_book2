8776975b6ef0c3cf30e5951ff4d3656cc7369649
mongopa <mongopa88@gmail.com>
2018-06-16 22:04:33 +0900

    convert_length 2th

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,5 @@
-def convert_length(lenght, unit_form, unit_to)
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37}
-  (lenght/ units[unit_form]* units[unit_to]).round(2)
+UNITS = { :m => 1.0, :ft => 3.28, :in => 39.37}
+
+def convert_length(lenght, from: :m, to: :m)
+  (lenght/ UNITS[from]* UNITS[to]).round(2)
 end
