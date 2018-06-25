5b609bf9508bf7c11d732ef372c29ce1478d1960
mongopa <mongopa88@gmail.com>
2018-06-25 20:47:46 +0900

    deep_freeze 2th

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,5 +2,5 @@ require '../lib/deep_freezable'
 
 class Bank
   extend DeepFreezable
-  COUNTRIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
+  CURRENCIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
 end
